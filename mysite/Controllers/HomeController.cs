47eaef7f10257323ecff03180eae262421adaa7a
likai <likai_ms@outlook.com>
2018-05-24 17:08:26 +0800

    update

--- mysite/Controllers/HomeController.cs
+++ mysite/Controllers/HomeController.cs
@@ -17,6 +17,26 @@ namespace mysite.Controllers
         {
             return View();
         }
+        /// <summary>
+        /// 博文
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Article()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 图集
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 关于我
+        /// </summary>
+        /// <returns></returns>
         public ActionResult About()
         {
             return View();
