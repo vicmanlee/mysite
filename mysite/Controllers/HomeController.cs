ab1c8bd80adb160a5fe6aceb0cbfb649d4dd894f
likai <likai_ms@outlook.com>
2018-05-24 11:05:03 +0800

    update

--- mysite/Controllers/HomeController.cs
+++ mysite/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace mysite.Controllers
 {
     public class HomeController : Controller
     {
+        IDbConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["SiteDB"].ConnectionString);
         // GET: Home
         public ActionResult Index()
         {
